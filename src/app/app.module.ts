36e00d19f42f9b428b98591ea5fe02cfcfd01c5c
russellomo <omo.russell@gmail.com>
2017-10-29 09:02:00 -1000

    Added basic CRUD for project layer
    -added angularfire to package.json
    -added firebase provider
    - placed code in home for test functionalities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,21 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseProvider } from '../providers/firebase/firebase';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBa-3KkWI1xUxVeqQG2r1T4_fS0MBUX75U",
+  authDomain: "rambutan-b6fc2.firebaseapp.com",
+  databaseURL: "https://rambutan-b6fc2.firebaseio.com",
+  projectId: "rambutan-b6fc2",
+  storageBucket: "rambutan-b6fc2.appspot.com",
+  messagingSenderId: "172585513927"
+};
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -17,6 +32,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
@@ -28,7 +46,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FirebaseProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
+import { FirebaseProvider } from './../../providers/firebase/firebase';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  shoppingItems: Observable<any[]>;
+  newItem = {
+                name: '',
+                events: ''
+            };
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public firebaseProvider: FirebaseProvider) {
+    this.shoppingItems = this.firebaseProvider.getProjects();
+    this.shoppingItems.forEach(project => {
+      console.log(project);
+    })
+    //console.log(this.shoppingItems);
+    //this.shoppingItems.subscribe(val => console.log(val));
   }
 
+  addItem() {
+    this.firebaseProvider.addProject(this.newItem);
+  }
+
+  removeItem(item) {
+    console.log(item);
+    this.firebaseProvider.removeProject(item);
+  }
 }
