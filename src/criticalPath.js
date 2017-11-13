f1539ed95275d2e72621d174d71d76bb7294829e
Jeanclyde Cruz <ignis14311@gmail.com>
2017-11-13 03:25:09 -1000

    fowardpass actually works now
    todo, backwardpass,calculate totalfloat

--- src/criticalPath.js
+++ src/criticalPath.js
@@ -49,7 +49,7 @@ var activityD={
 
 var activityF={
   name:"F",
-  duration:3,
+  duration:15,
   description:"asdf",
   dependencies:[activityA],
   assignees:["aaa","bbb","ccc"],
@@ -77,15 +77,75 @@ var activityE={
   assignees:["aaa","bbb","ccc"],
 }
 exampleActivitySet.push(activityA);
+exampleActivitySet.push(activityF);
 exampleActivitySet.push(activityB);
+exampleActivitySet.push(activityH);
+exampleActivitySet.push(activityG);
 exampleActivitySet.push(activityC);
 exampleActivitySet.push(activityD);
 exampleActivitySet.push(activityE);
-exampleActivitySet.push(activityF);
-exampleActivitySet.push(activityG);
-exampleActivitySet.push(activityH);
 //console.log(exampleActivitySet);
 //endregion
+
+var exampleActivitySet2=[
+  { name: 'A',
+  duration: 10,
+  description: 'asdf',
+  dependencies: [],
+  assignees: [ 'aaa', 'bbb', 'ccc' ],
+  earliestStart: 0,
+  earliestEnd: 10 },
+  { name: 'F',
+    duration: 15,
+    description: 'asdf',
+    dependencies: [],
+    assignees: [ 'aaa', 'bbb', 'ccc' ],
+    earliestStart: 10,
+    earliestEnd: 25 },
+  { name: 'B',
+    duration: 20,
+    description: 'asdf',
+    dependencies: [],
+    assignees: [ 'aaa', 'bbb', 'ccc' ],
+    earliestStart: 10,
+    earliestEnd: 30 },
+  { name: 'H',
+    duration: 15,
+    description: 'asdf',
+    dependencies: [],
+    assignees: [ 'aaa', 'bbb', 'ccc' ],
+    earliestStart: 10,
+    earliestEnd: 25 },
+  { name: 'G',
+    duration: 5,
+    description: 'asdf',
+    dependencies: [],
+    assignees: [ 'aaa', 'bbb', 'ccc' ],
+    earliestStart: 35,
+    earliestEnd: 40 },
+  { name: 'C',
+    duration: 5,
+    description: 'asdf',
+    dependencies: [],
+    assignees: [ 'aaa', 'bbb', 'ccc' ],
+    earliestStart: 30,
+    earliestEnd: 35 },
+  { name: 'D',
+    duration: 10,
+    description: 'asdf',
+    dependencies: [],
+    assignees: [ 'aaa', 'bbb', 'ccc' ],
+    earliestStart: 35,
+    earliestEnd: 45 },
+  { name: 'E',
+    duration: 20,
+    description: 'asdf',
+    dependencies: [],
+    assignees: [ 'aaa', 'bbb', 'ccc' ],
+    earliestStart: 45,
+    earliestEnd: 65 },];
+
+const cp = require('./providers/cp/cp');
 
 //calculates the longest path
 function longestPath(startNode){
@@ -118,7 +178,7 @@ function longestPath(startNode){
 }
 
 //calculate the shortest distance.
-function shortestPath(startNode){
+function shortestPath(firstStartNode){
   var paths=[];
   var names=[];
 
@@ -136,12 +196,12 @@ function shortestPath(startNode){
     for(var i=0;i<startNode.dependencies.length;i++){//for each edge (dependencies) of the node...
       var child=startNode.dependencies[i];
       names.push(startNode.name);
-      findAllPaths(startNode.dependencies[i],currentCost+child.duration);//recursively find the next node until base case is satisfied
+      findAllPaths(child,currentCost+child.duration);//recursively find the next node until base case is satisfied
     }
   }//end findAllPaths function
 
 
-  findAllPaths(startNode,startNode.duration);
+  findAllPaths(firstStartNode,firstStartNode.duration);
   //console.log(paths);
   console.log(names);
   return Math.min.apply(Math,paths);
@@ -152,6 +212,251 @@ function shortestPath(startNode){
 //objects have dependencies that are *before* the node, hence
 //instead of usually putting the head of the node into a longest
 //path algorithm, the last node is put in instead
-console.log('shortest path:'+shortestPath(activityE));
-console.log('longest path:'+longestPath(activityE));
+//console.log('shortest path:'+shortestPath(activityE));
+//console.log('longest path:'+longestPath(activityE));
 
+function calculateTimes(topoEventSet){
+  var forwardPassResult=forwardPassCalculation(topoEventSet);
+  //console.log(forwardPassResult);
+  forwardPassResult=this.cpProvider.topologicalSort(forwardPassResult);
+  var backwardPassResult=backwardPassCalculation(forwardPassResult);
+}
+
+
+//forward pass calculation, calculates the earliest times for each of the nodes, adding them as properties
+function forwardPassCalculation(topoEventSet){
+  //console.log(topoEventSet);
+  var originalSet=clone(topoEventSet);//keeps an original record of dependencies
+  var finishedNodes=[];
+  var inProcessNodes=[];
+
+  //initialize the first node
+  inProcessNodes.push(topoEventSet.shift());
+
+
+  while(inProcessNodes.length>0){
+    var nodeU=inProcessNodes.shift();
+    if(!nodeU.hasOwnProperty('earliestStart')){nodeU.earliestStart=0};//case for the first node
+    nodeU.earliestEnd=nodeU.earliestStart+nodeU.duration;//calculate the earliest end time
+    //console.log(nodeU);
+    //console.log(inProcessNodes);
+
+
+    for(var i=0;i<topoEventSet.length;i++){//for each node in the topologically sorted node set...
+      var nodeV=topoEventSet[i];
+
+
+      if(checkForDependencyMatch(nodeV,nodeU)){//For each vertex v directly following u...
+        //calculate the earliest start times...
+        if(!nodeV.hasOwnProperty('earliestStart')){//if earliest start time isn't initialized yet...
+          //console.log(nodeV);
+          nodeV.earliestStart=nodeU.earliestEnd;//calculate start time
+        }else if(nodeU.earliestEnd>nodeV.earliestStart){//else, compare start times (case where there are multiple edges
+          nodeV.earliestStart=nodeU.earliestEnd;
+        }
+
+        //calculate the earliest end time...
+        nodeV.earliestEnd=nodeV.earliestStart+nodeV.duration;
+        ///console.log(nodeV);
+
+
+        //remove the edge/dependency
+        var index=topoEventSet[i].dependencies.indexOf(nodeU);
+        topoEventSet[i].dependencies.splice(index,1);
+
+        //push nodeV into inProcessNodes to turn into nodeU's
+        inProcessNodes.push(nodeV);
+      }
+    }
+
+    //checks if node already has been calculated due to multiple dependencies, might have an updated earliestStart/earliestEnd
+    if(contains(finishedNodes,nodeU)){
+      //console.log(nodeU);
+      var index=finishedNodes.indexOf(nodeU);
+      finishedNodes.splice(index,1,nodeU);
+      //console.log(finishedNodes);
+    }else{
+      finishedNodes.push(nodeU);
+    }
+
+
+
+    //console.log(inProcessNodes.length);
+  }
+
+
+  //add back dependencies
+  for(var i=0;i<finishedNodes.length;i++){
+    //console.log(i);
+    var node=finishedNodes[i];
+    //console.log(originalSet);
+    var temp=containsName(originalSet,node);
+    finishedNodes[i].dependencies=temp.dependencies;
+
+  }
+
+  //console.log(finishedNodes);
+  //console.log(originalTopoEventSet);
+  return finishedNodes;
+}
+
+//backward pass calculation, calculates the latest times for each of the nodes, adding them as properties
+function backwardPassCalculation(forwardPassResult){//needs
+  forwardPassResult.reverse();
+  var finishedNodes=[];
+  var inProcessNodes=[];
+
+  //initialize the first node (which is the last node, starting from the 'right)
+  inProcessNodes.push(forwardPassResult.shift());
+  //console.log(inProcessNodes);
+
+
+  while(inProcessNodes.length>0){
+    var nodeU=inProcessNodes.shift();
+    //console.log(nodeU);
+    if(!nodeU.hasOwnProperty('latestEnd')){nodeU.latestEnd=nodeU.earliestEnd};//case for the first node, property 'earliest end should already be initialized' (because of the forwardPassCalculation
+    nodeU.latestStart=nodeU.latestEnd-nodeU.duration;//calculate the earliest end time
+    //console.log(nodeU);
+    //console.log(inProcessNodes);
+
+
+    for(var i=0;i<forwardPassResult.length;i++){//for each node in the topologically sorted node set... (working/counting backwards)
+      var nodeV=forwardPassResult[i];
+
+      //console.log(nodeU);
+      //console.log(checkForDependencyMatch(nodeU,nodeV));
+      if(checkForDependencyMatch(nodeU,nodeV)){//For each vertex v directly following u...
+        //calculate the latest end times...
+        if(!nodeV.hasOwnProperty('latestStart')){//if latest start time isn't initialized yet...
+          //console.log(nodeV);
+          nodeV.latestEnd=nodeU.latestStart;//calculate latest end time
+        }else if(nodeU.latestStart>nodeV.latestEnd){//else, compare start times (case where there are multiple edges
+          nodeV.latestEnd=nodeU.latestStart;
+        }
+
+        //calculate the latest start time...
+        nodeV.latestStart=nodeV.latestEnd-nodeV.duration;
+        ///console.log(nodeV);
+
+
+        //remove the edge/dependency
+        var index=nodeU.dependencies.indexOf(nodeV);
+        nodeU.dependencies.splice(index,1);
+
+        //push nodeV into inProcessNodes to turn into nodeU's
+        inProcessNodes.push(nodeV);
+        console.log(inProcessNodes.length);
+      }
+    }
+
+    //checks if node already has been calculated due to multiple dependencies, might have an updated earliestStart/earliestEnd
+    if(contains(finishedNodes,nodeU)){
+      //console.log(nodeU);
+      var index=finishedNodes.indexOf(nodeU);
+      finishedNodes.splice(index,1,nodeU);
+      //console.log(finishedNodes);
+    }else{
+      //console.log(nodeU);
+      finishedNodes.push(nodeU);
+    }
+
+    //console.log(inProcessNodes.length);
+  }
+
+  //console.log(finishedNodes);
+  return finishedNodes;
+}
+
+//given object1, checks if object2 exist in object1's dependency list
+function checkForDependencyMatch(object1,object2){
+  for(var i=0;i<object1.dependencies.length;i++){
+    //console.log('checking');
+    //console.log(object1.dependencies[i]);
+    if(object1.dependencies[i]===object2)
+      //console.log('match');
+      return true;
+    }
+  return false;
+}
+
+//checks if an array has obj in it
+function contains(a, obj) {
+  for (var i = 0; i < a.length; i++) {
+    if (a[i] === obj) {
+      return a[i];
+    }
+  }
+  return false;
+}
+
+//weak comparison
+function containsName(a,obj){
+  for (var i = 0; i < a.length; i++) {
+    if (a[i].name === obj.name) {
+      return a[i];
+    }
+  }
+  return false;
+}
+
+
+
+//cloning objects
+//https://davidwalsh.name/javascript-clone
+function clone(src) {
+  function mixin(dest, source, copyFunc) {
+    var name, s, i, empty = {};
+    for(name in source){
+      // the (!(name in empty) || empty[name] !== s) condition avoids copying properties in "source"
+      // inherited from Object.prototype.	 For example, if dest has a custom toString() method,
+      // don't overwrite it with the toString() method that source inherited from Object.prototype
+      s = source[name];
+      if(!(name in dest) || (dest[name] !== s && (!(name in empty) || empty[name] !== s))){
+        dest[name] = copyFunc ? copyFunc(s) : s;
+      }
+    }
+    return dest;
+  }
+
+  if(!src || typeof src != "object" || Object.prototype.toString.call(src) === "[object Function]"){
+    // null, undefined, any non-object, or function
+    return src;	// anything
+  }
+  if(src.nodeType && "cloneNode" in src){
+    // DOM Node
+    return src.cloneNode(true); // Node
+  }
+  if(src instanceof Date){
+    // Date
+    return new Date(src.getTime());	// Date
+  }
+  if(src instanceof RegExp){
+    // RegExp
+    return new RegExp(src);   // RegExp
+  }
+  var r, i, l;
+  if(src instanceof Array){
+    // array
+    r = [];
+    for(i = 0, l = src.length; i < l; ++i){
+      if(i in src){
+        r.push(clone(src[i]));
+      }
+    }
+    // we don't clone functions for performance reasons
+    //		}else if(d.isFunction(src)){
+    //			// function
+    //			r = function(){ return src.apply(this, arguments); };
+  }else{
+    // generic objects
+    r = src.constructor ? new src.constructor() : {};
+  }
+  return mixin(r, src, clone);
+
+}
+
+
+//forwardPassCalculation(exampleActivitySet);//okay i think this works
+//backwardPassCalculation(exampleActivitySet);//should only works with forwardPassCalculation result
+calculateTimes(exampleActivitySet);
+//console.log(forwardPassCalculation((exampleActivitySet)));
--- src/criticalPathOld.js
+++ src/criticalPathOld.js
@@ -32,22 +32,79 @@ shortest path algorithm, maybe be adapted to do the opposite
                 p[v] ← u.
  */
 
-//each node will need..
-//name
-//duration*
-//description
-//dependencies*
-//assignees
-
-//eventSet is a list/array of node objects
-
-var nodeObjectExample={
-  name:"name",
-  duration:1,
+//region example set
+//example set to test with
+var exampleActivitySet=[];
+var activityA={
+  name:"A",
+  duration:10,
   description:"asdf",
-  dependencies:["a","b","c"],
+  dependencies:[],
   assignees:["aaa","bbb","ccc"],
 }
+var activityB={
+  name:"B",
+  duration:20,
+  description:"asdf",
+  dependencies:[activityA],
+  assignees:["aaa","bbb","ccc"],
+}
+var activityC={
+  name:"C",
+  duration:5,
+  description:"asdf",
+  dependencies:[activityB],
+  assignees:["aaa","bbb","ccc"],
+}
+var activityD={
+  name:"D",
+  duration:10,
+  description:"asdf",
+  dependencies:[activityC],
+  assignees:["aaa","bbb","ccc"],
+}
+
+var activityF={
+  name:"F",
+  duration:3,
+  description:"asdf",
+  dependencies:[activityA],
+  assignees:["aaa","bbb","ccc"],
+}
+var activityG={
+  name:"G",
+  duration:5,
+  description:"asdf",
+  dependencies:[activityF,activityC],
+  assignees:["aaa","bbb","ccc"],
+}
+var activityH={
+  name:"H",
+  duration:15,
+  description:"asdf",
+  dependencies:[activityA],
+  assignees:["aaa","bbb","ccc"],
+}
+
+var activityE={
+  name:"E",
+  duration:20,
+  description:"asdf",
+  dependencies:[activityD,activityG,activityH],
+  assignees:["aaa","bbb","ccc"],
+}
+exampleActivitySet.push(activityA);
+exampleActivitySet.push(activityB);
+exampleActivitySet.push(activityC);
+exampleActivitySet.push(activityD);
+exampleActivitySet.push(activityE);
+exampleActivitySet.push(activityF);
+exampleActivitySet.push(activityG);
+exampleActivitySet.push(activityH);
+//console.log(exampleActivitySet);
+//endregion
+
+
 
 function calculateStartEndtimes(eventSet){
   var forwardPass=forwardPassCalculation((eventSet));
@@ -61,40 +118,79 @@ function calculateStartEndtimes(eventSet){
 function forwardPassCalculation(eventSet) {
   var calculatedNodes=[];
   var noIncomingEdgeSet=[];
-  noIncomingEdgeSet.push(eventSet.forEach(checkForDependencies()));
+  noIncomingEdgeSet=checkForNonDependencies(eventSet);//pushes nodes dont have any edges
+  //console.log(noIncomingEdgeSet);
 
-  while(noIncomingEdgeSet!=null){
-    var node=noIncomingEdgeSet.pop()
+  // console.log(node);
+  //calculates beginning and end times, and adds as a property
+  while(noIncomingEdgeSet.length>0){
 
-    //calculates beginning and end times, and adds as a property
-    if(node.earliestStart==null)node.earliestStart=0;//if not null, it was already added when checking for dependency
+    //node 'n'
+    var node=noIncomingEdgeSet.shift();// .shift takes from the first index
+    if(node.dependencies.length===0){node.earliestStart=0}
     node.earliestEnd=node.earliestStart+node.duration;
-
-    calculatedNodes.push(node)
-
-
+    calculatedNodes.push(node);
     //check node dependency and remove edges
-    for(m=0;m<eventSet.length;m++){//scans for dependencies
-      if (eventSet[m].dependencies[node.name]) {//if a node is found as a dependent...
+    for(var m=0;m<eventSet.length;m++){//for each node...
+      //console.log(eventSet[m]);
+      //console.log('comparing');
+      //console.log(eventSet[m]);
+      //console.log('with');
+      //console.log(node);
+      //console.log(m);
+      if (checkForDependencyMatch(eventSet[m],node)) {//if node eventSet[m] connects with 'node' (n)..
+        if(eventSet[m].earliestStart===null){//case where earliest start property hasnt been initialized
+          eventSet[m].earliestStart=node.earliestEnd;
+        }else if(eventSet[m].earliestStart<node.earliestEnd){//if the eventSet[m] node start earlier than 'node' 'n', replace the starting time
+          eventSet[m].earliestStart=node.earliestEnd;
+        }
 
-        //case check, where more than one edge is found
-        if(eventSet[m].earliestStart==null)eventSet[m].earliestStart=node.earliestEnd;
-        else if(eventSet[m].earliestStart<node.earliestEnd)eventSet[m].earliestStart=node.earliestEnd;//if the current node start earlier than the current node, replace the starting time
 
-        delete eventSet[m].dependencies[node.name];//remove the edge
-        if (eventSet[m].dependencies == null) {//if m has no other incoming edges then
+        //console.log('dependency match');
+        //console.log(eventSet[m]);
+
+
+
+        //delete eventSet[m].dependencies[node];//remove the edge
+        //console.log(eventSet[m].dependencies.indexOf(node));
+        var index=eventSet[m].dependencies.indexOf(node);
+        eventSet[m].dependencies.splice(index,1);
+        //eventSet[m].dependencies[node]=null;
+
+        //console.log(m);
+        //console.log(eventSet[m]);
+        if (eventSet[m].dependencies.length>0) {//if m has no other incoming edges then
           noIncomingEdgeSet.push(eventSet[m])//insert m into noIncomingEdgeSet
         }
+        //console.log(eventSet[m]);
       }
     }
   }
 
   //checks for loops/cycles, otherwise returns the same set of nodes with added properties
-  for(i=0;i<eventSet.length;i++){
-    if(eventSet[i].dependencies!=null)return 'wut';
-    else return calculatedNodes;
+  for(var i=0;i<eventSet.length;i++){
+    if(eventSet[i].dependencies.length>0){
+      console.log('still has dependencies')
+      console.log(eventSet[i]);
+      //return 'wut';
+    }
   }
+  //console.log(calculatedNodes);
+  return calculatedNodes;
 }
+
+function checkForDependencyMatch(object1,object2){
+  for(var i=0;i<object1.dependencies.length;i++){
+    //console.log('checking');
+    //console.log(object1.dependencies[i]);
+    if(object1.dependencies[i]===object2){
+      return true;
+    }
+  }
+  return false;
+}
+
+
 
 //basically like the forward pass, but will go backwards going through child nodes first
 function backwardPassCalculation(eventSet){
@@ -106,7 +202,7 @@ function backwardPassCalculation(eventSet){
     var node=incomingEdgeSet.pop();
 
     //calculates beginning and end times, and adds as a property
-    if(node.latestEnd==null)node.latestEnd=node.earliestEnd;//if not null, it was already added when checking for dependency
+    if(node.latestEnd===null)node.latestEnd=node.earliestEnd;//if not null, it was already added when checking for dependency
     node.latestStart=node.latestEnd-node.duration;
 
     calculatedNodes.push(node);
@@ -136,115 +232,57 @@ function backwardPassCalculation(eventSet){
   }
 }
 
-
-
-
-//why do i even need this
-function KahnAlgorithm(eventSet){
-  var L=[];//L ← Empty list that will contain the sorted elements
-
-  var S=[];//S ← Set of all nodes with no incoming edge
-  S.push(eventSet.forEach(checkForDependencies))
-
-
-  while(S!=null) {//while S is non-empty do
-    var node = S.pop()//remove a node n from S
-    L.push(node)//add n to tail of L
-
-    for (m = 0; m < eventSet.length; m++) {//for each node m with...
-      if (eventSet[m].dependencies[node.name]) {// an edge e from n to m do...
-        delete eventSet[m].dependencies[node.name];//remove edge e from the graph
-        if (eventSet[m].dependencies == null) {//if m has no other incoming edges then
-          S.push(eventSet[m])//insert m into S
-        }
-      }
+function checkForDependencies(nodeSet){
+  var list=[];
+  for(var i=0;i<nodeSet.length;i++){
+    if(nodeSet[i].dependencies.length>=0){
+      list.push(nodeSet[i]);
     }
   }
+  return list;
+}
 
-  for(node=0;node<eventSet.length();node++){
-    if(eventSet[node].dependencies!=null){//if graph has edges then
-      return 'wut happened';//return error (graph has at least one cycle)
-    }else{//else
-      return L;//return L (a topologically sorted order)
+function checkForNonDependencies(nodeSet){
+  var list=[];
+  for(var i=0;i<nodeSet.length;i++){
+    if(nodeSet[i].dependencies.length===0){
+      list.push(nodeSet[i]);
     }
   }
-
+  return list;
 
 }
 
-function checkForDependencies(node){
- if(node.dependencies==null)return node;
- else return node.dependencies;
-}
+console.log(forwardPassCalculation(exampleActivitySet));
 
 
 
-var exampleActivitySet=[];
-var activityA={
-  name:"A",
-  duration:10,
-  description:"asdf",
-  dependencies:[],
-  assignees:["aaa","bbb","ccc"],
-}
-var activityB={
-  name:"B",
-  duration:20,
-  description:"asdf",
-  dependencies:["A"],
-  assignees:["aaa","bbb","ccc"],
-}
-var activityC={
-  name:"C",
-  duration:5,
-  description:"asdf",
-  dependencies:["B"],
-  assignees:["aaa","bbb","ccc"],
-}
-var activityD={
-  name:"D",
-  duration:10,
-  description:"asdf",
-  dependencies:["C"],
-  assignees:["aaa","bbb","ccc"],
-}
-var activityE={
-  name:"E",
-  duration:20,
-  description:"asdf",
-  dependencies:["D","G","H"],
-  assignees:["aaa","bbb","ccc"],
-}
-var activityF={
-  name:"15",
-  duration:3,
-  description:"asdf",
-  dependencies:["A"],
-  assignees:["aaa","bbb","ccc"],
-}
-var activityG={
-  name:"G",
-  duration:5,
-  description:"asdf",
-  dependencies:["F","C"],
-  assignees:["aaa","bbb","ccc"],
-}
-var activityH={
-  name:"H",
-  duration:15,
-  description:"asdf",
-  dependencies:["A"],
-  assignees:["aaa","bbb","ccc"],
-}
 
-exampleActivitySet.push(activityA);
-exampleActivitySet.push(activityB);
-exampleActivitySet.push(activityC);
-exampleActivitySet.push(activityD);
-exampleActivitySet.push(activityE);
-exampleActivitySet.push(activityF);
-exampleActivitySet.push(activityG);
-exampleActivitySet.push(activityH);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function findPaths(activitySet,beginningActivity,endingActivity){
   //base case
