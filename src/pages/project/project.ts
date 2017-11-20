72d756c109d414d870bf11bf2441a20c06b2b730
russellomo <omo.russell@gmail.com>
2017-11-19 18:33:26 -1000

    adding in dependency selection on the project page

--- src/pages/project/project.ts
+++ src/pages/project/project.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseProvider } from './../../providers/firebase/firebase';
+import { ActivityFormPage } from '../activityForm/activityForm';
 import { Observable } from 'rxjs/Observable';
 import { AlertController } from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { DurationValidator } from  '../../validators/duration';
-
-
 
 
 /**
@@ -23,39 +20,110 @@ import { DurationValidator } from  '../../validators/duration';
 })
 export class ProjectPage {
   events: Observable<any[]>;
+  dependencies: any[];
   item: any;
   newEvent = {
     name: '',
     description: '',
-    duration: ''
+    duration: null
   };
 
-  activityForm: FormGroup;
 
-  submitAttempt: boolean = false;
-
-
-  constructor(public navCtrl: NavController,public firebaseProvider: FirebaseProvider, public navParams: NavParams,public alertCtrl: AlertController, public formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController,public firebaseProvider: FirebaseProvider, public navParams: NavParams,public alertCtrl: AlertController) {
     this.item = navParams.get('item');
+    console.log(this.item);
     this.events = this.firebaseProvider.getEvents(this.item);
     this.events.forEach(event => {
       console.log(event);
-    })
-    this.activityForm = formBuilder.group({
-      name:['', Validators.compose([Validators.maxLength(50),Validators.required, Validators.pattern('[a-zA-Z 0-9.!@#$%^&*()_+-=]*'), Validators.required])],
-      description:['',Validators.compose([Validators.maxLength(100), Validators.required, Validators.pattern('[a-zA-Z 0-9.!@#$%^&*()_+-=]*'), Validators.required])],
-      duration:['', Validators.compose([Validators.required, DurationValidator.isValid])]
     });
+    this.dependencies = [];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProjectPage');
   }
 
+  // add to the dependency array.
+  setDependency(dependency){
+    console.log(dependency);
+    let exists = false;
+    for(let x = 0; x < this.dependencies.length; x++) {
+      if(dependency.name == this.dependencies[x].name) {
+        exists = true;
+        break;
+      }
+    }
+    if(exists) {
+      const alert = this.alertCtrl.create({
+        title: 'Alert',
+        message: 'This dependency already exists',
+        buttons: [
+          {
+            text: 'Okay',
+            role: 'Okay',
+            handler: () => {
+
+            }
+          },
+        ]
+      });
+      alert.present();
+    }else {
+      this.dependencies.push({name: dependency.name, duration: dependency.duration});
+      console.log(this.dependencies);
+    }
+  }
+
   addActivity(item){
-    this.submitAttempt = true;
-    this.firebaseProvider.addActivity(item, this.newEvent);
-    // this.submitAttempt = false;
+    if(this.newEvent.name && this.newEvent.duration) {
+      const parsedDuration = parseInt(this.newEvent.duration);
+      if(parsedDuration) {
+        this.newEvent.duration = parsedDuration
+        const ref = this.firebaseProvider.addActivity(item, this.newEvent);
+
+        for(let x = 0; x < this.dependencies.length; x++) {
+          this.firebaseProvider.addDependency(this.item, ref, this.dependencies[x]);
+        }
+        //reset the fields
+        this.newEvent = {
+          name: '',
+          description: '',
+          duration: null
+        };
+        this.dependencies = [];
+      } else {
+        const alert = this.alertCtrl.create({
+          title: 'Alert',
+          message: 'In order to add an activity, the duration must be single integer',
+          buttons: [
+            {
+              text: 'Okay',
+              role: 'Okay',
+              handler: () => {
+
+              }
+            },
+          ]
+        });
+        alert.present();
+      }
+    }else {
+      const alert = this.alertCtrl.create({
+        title: 'Alert',
+        message: 'In order to add an activity, it must have both a name and duration in days!',
+        buttons: [
+          {
+            text: 'Okay',
+            role: 'Okay',
+            handler: () => {
+
+            }
+          },
+        ]
+      });
+      alert.present();
+    }
+
   }
   removeActivity(item) {
     console.log(item);
